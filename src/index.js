69b8f881e0197abd1eaaca3e87093bb70d68080b
arthurchic20 <arthurchic20@gmail.com>
2023-01-19 13:27:44 +0600

    init: start project revers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    const res = String(number).split('');
+    if (res[0] === '-') res.shift()
+    let revArr = res.reverse();
+    return revArr.join('');
 }
